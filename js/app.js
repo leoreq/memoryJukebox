52133fe7e42f3fab4c72e72a183c6d7d0b66d48b
leoreq <leoreq@gmail.com>
2015-05-03 21:26:14 -0400

    variable sound player add

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //quiz question array that will be global variables
     var questions = [{
-        song: "#BYTHEWAY",
+        song: "ByTheWay",
+        file:"sound/ByTheWay.mp3",
         choices: ["Got the Life : KoRN", "Crawling : Linkin Park", "By The Way : Red hot Chilli Peppers", "Nookie : Limp Bizkit"],
         qNum : 0,
         correct : "By The Way : Red hot Chilli Peppers",
@@ -11,6 +12,10 @@
 function update() {
 $('#messageBox').text('Let the games begin');
 $(".choicesSection ul li").remove();
+console.log('<audio id="'+questions[0].song+'" controls src="' + questions[0].file + '"></audio>');
+$('.displaySection')
+                .append('<audio id="'+questions[0].song+'" controls src="' + questions[0].file + '"></audio>');		
+
 for(var i = 0; i < questions[0].choices.length; i++){
                 console.log("DEBUG:the option "+questions[0].choices[i]+" compared with "+questions[0].correct+"will be "+(questions[0].choices[i]==questions[0].correct)+" ");
                 bstatus="INCORRECT";
@@ -44,8 +49,9 @@ $("#mjButtons").on("click", "#playbutton", function () {
         $(".collectionSection").css("display", "inline");
         //update question info
         update();
-        console.log(songPlayer(questions[0].song));
-        songPlayer(questions[0].song);
+        musicTag="#"+questions[0].song+"";
+        console.log(musicTag);
+        songPlayer(musicTag);
         //var newQuestion = '<span class="question">'+questions[currentQuestion].question+'</span><br><div id="answer_holder"><input type="radio" name="option" class="option" value="0"><span class="answer">'+questions[currentQuestion].choices[0]+'</span><br><input type="radio" name="option" class="option" value="1"><span class="answer">'+questions[currentQuestion].choices[1]+'</span><br><input type="radio" name="option" class="option" value="2"><span class="answer">'+questions[currentQuestion].choices[2]+'</span><br><input type="radio" name="option" class="option" value="3"><span class="answer">'+questions[currentQuestion].choices[3]+'</span><br></div><div id="button_holder"><input type="button" id="submit" value="Submit Answer"><span id="hint"></span><input type="button" id="retry_button" value="Try Again!"></div>';
         //$("#question_wrapper").html(newQuestion);
         //$("#last_question_fact").html("");
@@ -59,8 +65,9 @@ function playHadouken () {
 }
 
 function songPlayer (tag) {
+	console.log(tag+" was played");
 	$(tag)[0].volume = 0.5;
 	$(tag)[0].load();
 	$(tag)[0].play();
-	console.log($(tag)+" was played");
+	
 }
