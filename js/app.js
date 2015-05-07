95c061a27a175d8c836f0be84ed990f393d41e07
leoreq <leoreq@gmail.com>
2015-05-06 23:56:20 -0400

    addign song

--- js/app.js
+++ js/app.js
@@ -1,31 +1,23 @@
 //quiz question array that will be global variables
-    var questions = [
-{correctArtist:'U2 ',correctSong:'“Elevation”',file: "sound/Elevation.mp3",image: "url('./images/U2.jpg')",correct:'U2 – “Elevation”',choices:['Nirvana – “Smells Like Teen Spirit”','U2 – “Elevation”','R.E.M. – “Losing My Religion”','Pearl Jam – “Jeremy”','Alanis Morissette – “You Oughta Know”'],song:"Elevation",prize: "url('./images/recordU2.jpg')"}
-,
-{correctArtist:'Red Hot Chili Peppers ',correctSong:'“By The Way”',file:"sound/ByTheWay.mp3",image: "url('./images/RHCPband.jpg')",correct:'Red Hot Chili Peppers – “By The Way”',choices:['Red Hot Chili Peppers – “By The Way”','Metallica – “One”','Beastie Boys – “Sabotage”','Beck – “Loser”','Soundgarden – “Black Hole Sun”'],song:"ByTheWay",prize: "url('./images/recordRHCP.JPG')"}
-,
-{correctArtist:'Metallica ',correctSong:'“Nothing Else Matters”',file: "sound/NothingElseMatters.mp3",image: "url('./images/METALLICAband.jpg')",correct:'Metallica – “Nothing Else Matters”',choices:['Eminem – “My Name Is”','Counting Crows – “Mr. Jones”','Radiohead – “Creep”','Metallica – “Nothing Else Matters”','Oasis – “Wonderwall”'],song:"NothingElseMatters",prize: "url('./images/recordMETALLICA.jpg')"}
-,
-{correctArtist:'Korn ',correctSong:'“Freak On A Leash”',file: "sound/FreakOnALeash.mp3",image: "url('./images/KORNband.jpg')",correct:'Korn – “Freak On A Leash”',choices:['Green Day – “Good Riddance (Time of Your Life)”','Goo Goo Dolls – “Iris”','Collective Soul – “Shine”','Korn – “Freak On A Leash”','Alice in Chains – “Man in the Box”'],song:"FreakOnALeash",prize: "url('./images/recordKORN.jpg')"}
-,
-{correctArtist:'Blur ',correctSong:'“Song 2”',file: "sound/Song2.mp3",image: "url('./images/BLURband.jpg')",correct:'Blur – “Song 2”',choices:['Blur – “Song 2”','Sugar Ray – “Fly”','Weezer – “Buddy Holly”','Live – “I Alone”','Will Smith – “Gettin’ Jiggy Wit It”'],song:"Song2",prize: "url('./images/recordBLUR.jpg')"}
+var questions = [
+	{correctArtist:'U2 ',correctSong:'“Elevation”',file: "sound/Elevation.mp3",image: "url('./images/U2.jpg')",correct:'U2 – “Elevation”',choices:['Nirvana – “Smells Like Teen Spirit”','U2 – “Elevation”','R.E.M. – “Losing My Religion”','Pearl Jam – “Jeremy”','Alanis Morissette – “You Oughta Know”'],song:"Elevation",prize: "url('./images/recordU2.jpg')"}
+	,
+	{correctArtist:'Red Hot Chili Peppers ',correctSong:'“By The Way”',file:"sound/ByTheWay.mp3",image: "url('./images/RHCPband.jpg')",correct:'Red Hot Chili Peppers – “By The Way”',choices:['Red Hot Chili Peppers – “By The Way”','Metallica – “One”','Beastie Boys – “Sabotage”','Beck – “Loser”','Soundgarden – “Black Hole Sun”'],song:"ByTheWay",prize: "url('./images/recordRHCP.JPG')"}
+	,
+	{correctArtist:'Metallica ',correctSong:'“Nothing Else Matters”',file: "sound/NothingElseMatters.mp3",image: "url('./images/METALLICAband.jpg')",correct:'Metallica – “Nothing Else Matters”',choices:['Eminem – “My Name Is”','Counting Crows – “Mr. Jones”','Radiohead – “Creep”','Metallica – “Nothing Else Matters”','Oasis – “Wonderwall”'],song:"NothingElseMatters",prize: "url('./images/recordMETALLICA.jpg')"}
+	,
+	{correctArtist:'Korn ',correctSong:'“Freak On A Leash”',file: "sound/FreakOnALeash.mp3",image: "url('./images/KORNband.jpg')",correct:'Korn – “Freak On A Leash”',choices:['Green Day – “Good Riddance (Time of Your Life)”','Goo Goo Dolls – “Iris”','Collective Soul – “Shine”','Korn – “Freak On A Leash”','Alice in Chains – “Man in the Box”'],song:"FreakOnALeash",prize: "url('./images/recordKORN.jpg')"}
+	,
+	{correctArtist:'Blur ',correctSong:'“Song 2”',file: "sound/Song2.mp3",image: "url('./images/BLURband.jpg')",correct:'Blur – “Song 2”',choices:['Blur – “Song 2”','Sugar Ray – “Fly”','Weezer – “Buddy Holly”','Live – “I Alone”','Will Smith – “Gettin’ Jiggy Wit It”'],song:"Song2",prize: "url('./images/recordBLUR.jpg')"}
+    ]
 
-    /*{
-        song: "ByTheWay",
-        file:"sound/ByTheWay.mp3",
-        image:"url('./images/RHCPband.jpg')",
-        choices:['Red Hot Chili Peppers – “By The Way”','Metallica – “Enter Sandman”','Beastie Boys – “Sabotage”','Beck – “Loser”','Soundgarden – “Black Hole Sun”'],
-        qNum : 0,
-        correct : 'Red Hot Chili Peppers – “By The Way”',
-        correctSong: "“By The Way”",
-        correctArtist: "Red Hot Chili Peppers"
-        },
-	*/
-	
-        ]
 //Update function to be run after every iteration to update questions 
 function update() {
-	$('#messageBox').text('Let the games begin');
+	if(currentQuestion==0){$('#messageBox').text('Let the games begin');}
+	else{
+		displayQuestion=	currentQuestion+1;
+		$('#messageBox').text('This is question #'+displayQuestion);}
+		
 	$("#recordPlayer").css("background-image", "url('./images/player.JPG')");
 	$(".prizeSection").css("display", "none");
 	$(".choicesSection ul li").remove();
@@ -46,12 +38,14 @@ function update() {
 	        .prepend('<li><button class="bigbutton" id="'+bstatus+'">' + questions[currentQuestion].choices[i] + '</button></li>')		
 	    };
 	$(".choicesSection").css("display", "inline");
+	$(".collectionSection").css("display", "inline");
 	}
 
 
 function Prize() {
 	$('#messageBox2').text('CORRECTOU!!!');
 	$('#prizeMessage strong').text('You have won a new record!!');
+	$(".collectionSection").css("display", "none");//perhaps can be deleted
 	$(".choicesSection").css("display", "none");//perhaps can be deleted
 	$("#prizeHolder li").remove(); //Removes old prize
 	//prize will only change, so image displayed will be generic from class recordPrize
@@ -72,7 +66,8 @@ function Prize() {
 function Wrong() {
 
 	$('#messageBox2').text('NOPE');
-	$('#prizeMessage strong').text('NOPE');
+	$('#prizeMessage strong').text('You have not earned a record');
+	$('#prizeMessage ').css("margin-bottom", "3em");
 	$(".choicesSection").css("display", "none");//perhaps can be deleted
 	$("#prizeHolder li").remove(); //Removes old prize
 	//prize will only change, so image displayed will be generic from class recordPrize
@@ -86,65 +81,58 @@ function Wrong() {
 
 $(document).ready(function() {
 //actions that happen immediately after document is loaded
-
 console.log("ready");
-//Option to display information menu
 
-
-    
+//Instructions Listener
 $('#instructionsbutton')
 	.click(function(){
     	$("#MJinstructions").toggle();})
 
 //GameStart Listener 
 $("#mjButtons").on("click", "#playbutton", function () {
-        $("#playbutton").fadeOut(2000);
-        $("#diffbutton").fadeOut(2000);
-        //set the counters to zero
-        numberCorrect = 0;
-        currentQuestion = 0;
-
-        //update question info
-        update();
-
-        //var newQuestion = '<span class="question">'+questions[currentQuestion].question+'</span><br><div id="answer_holder"><input type="radio" name="option" class="option" value="0"><span class="answer">'+questions[currentQuestion].choices[0]+'</span><br><input type="radio" name="option" class="option" value="1"><span class="answer">'+questions[currentQuestion].choices[1]+'</span><br><input type="radio" name="option" class="option" value="2"><span class="answer">'+questions[currentQuestion].choices[2]+'</span><br><input type="radio" name="option" class="option" value="3"><span class="answer">'+questions[currentQuestion].choices[3]+'</span><br></div><div id="button_holder"><input type="button" id="submit" value="Submit Answer"><span id="hint"></span><input type="button" id="retry_button" value="Try Again!"></div>';
-        //$("#question_wrapper").html(newQuestion);
-        //$("#last_question_fact").html("");
+    $("#playbutton").fadeOut(2000);
+    $("#diffbutton").fadeOut(2000);
+    //set the counters to zero
+    numberCorrect = 0;
+    currentQuestion = 0;
+    //Load questions
+    update();
     });
 
 //GameNEXT Listener 
 $(".prizeSection").on("click", "#nextButton", function () {
-        if(currentQuestion<4) {
-	        currentQuestion += 1;
-	        console.log("current question is now"+currentQuestion);
-	        //Show collection
-	        $('.collectionSection').fadeIn(3000);
-	       	//update question info
-	        update();
-        }
-        else {
-        	$(".prizeSection").css("display", "none");
-        	$(".choicesSection").css("display", "none");
-        	$("#recordPlayer").css("display", "none");
-        	//Show collection
-        	$('#messageBox').text('END: YOUR FINAL SCORE IS!!!');
-	        $('.collectionSection').fadeIn(3000);
-        }
+    if(currentQuestion<4) {
+        currentQuestion += 1;
+        console.log("current question is now"+currentQuestion);
+        //Show collection
+        $('.collectionSection').fadeIn(3000);
+       	//update question info
+        update();
+    }
+    else {
+    	$(".prizeSection").css("display", "none");
+    	$(".choicesSection").css("display", "none");
+    	$("#recordPlayer").css("display", "none");
+    	//Show collection
+    	$('#messageBox').text('END: Your final score is : '+numberCorrect+' out of 5 questions');
+        $('.collectionSection').fadeIn(3000);
+    }
     });
+
 //CorrectAnswer Listener
 $(".choicesSection").on("click", "#CORRECT", function () {
-        //set the counters
-        numberCorrect += 1;
-        console.log("the answer is correct. Total correct answers are : "+numberCorrect);
-        //run Reward Program
-        Prize();
+    //set the counters
+    numberCorrect += 1;
+    console.log("the answer is correct. Total correct answers are : "+numberCorrect);
+    //run Reward Program
+    Prize();
     });
+
 //WrongAnswer Listener
 $(".choicesSection").on("click", "#INCORRECT", function () {
-     
-        console.log("the answer is incorrect. Total correct answers so far are : "+numberCorrect);
-        //run Reward Program
-        Wrong();
+    console.log("the answer is incorrect. Total correct answers so far are : "+numberCorrect);
+    //run noReward Program
+    Wrong();
     });
 });
 
